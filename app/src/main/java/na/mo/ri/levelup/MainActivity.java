0d4f9e568f2e52dd57c140cae0181af9e720610a
Jeean Lee <ljheeeve@gmail.com>
2018-11-30 00:56:25 +0900

    make group page with tab
    - 기본 탭 : 게시글/그룹원 랭킹/인증샷/나의 정보 구현
    -추가 탭 : 나의 정보쪽 탭은 my_info로 따로 분리해 구현
    - 게시판 구현 성공
    - 디자인 배치 형식만 함
    <Todo List>
    1. 그룹원 랭킹 따로 xml빼서 계속 추가할 수 있도록 구현
    2. 사진 업로드 구현
    3. 나의 정보쪽 탭 디자인 변경

--- app/src/main/java/na/mo/ri/levelup/MainActivity.java
+++ app/src/main/java/na/mo/ri/levelup/MainActivity.java
@@ -2,13 +2,62 @@ package na.mo.ri.levelup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TabHost;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    MyListAdapter myListAdapter;
+    ArrayList<list_item> list_itemArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //ddddd
+        listView = (ListView)findViewById(R.id.my_listview);
+
+        list_itemArrayList = new ArrayList<list_item>();
+
+        list_itemArrayList.add(
+                new list_item(R.mipmap.ic_launcher,"보라돌이","제목1",new Date(System.currentTimeMillis()),"내용1"));
+        list_itemArrayList.add(
+                new list_item(R.mipmap.ic_launcher,"뚜비","제목2",new Date(System.currentTimeMillis()),"내용2"));
+        list_itemArrayList.add(
+                new list_item(R.mipmap.ic_launcher,"나나","제목3",new Date(System.currentTimeMillis()),"내용3"));
+        list_itemArrayList.add(
+                new list_item(R.mipmap.ic_launcher,"뽀","제목4",new Date(System.currentTimeMillis()),"내용4"));
+        list_itemArrayList.add(
+                new list_item(R.mipmap.ic_launcher,"햇님","제목5",new Date(System.currentTimeMillis()),"내용5"));
+
+        myListAdapter = new MyListAdapter(MainActivity.this,list_itemArrayList);
+        listView.setAdapter(myListAdapter);
+
+        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1) ;
+        tabHost1.setup() ;
+        // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1") ;
+        ts1.setContent(R.id.post_content) ;
+        ts1.setIndicator("게시글") ;
+        tabHost1.addTab(ts1) ;
+        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
+        ts2.setContent(R.id.lank_content) ;
+        ts2.setIndicator("그룹원 랭킹") ;
+        tabHost1.addTab(ts2) ;
+        // 세 번째 Tab. (탭 표시 텍스트:"TAB 3"), (페이지 뷰:"content3")
+        TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3") ;
+        ts3.setContent(R.id.photo_content) ;
+        ts3.setIndicator("인증샷") ;
+        tabHost1.addTab(ts3) ;
+        // 네 번째 Tab. (탭 표시 텍스트:"TAB 4"), (페이지 뷰:"content4")
+        TabHost.TabSpec ts4 = tabHost1.newTabSpec("Tab Spec 4") ;
+        ts4.setContent(R.id.mypage_content) ;
+        ts4.setIndicator("나의 정보") ;
+        tabHost1.addTab(ts4) ;
+
+
     }
 }
---
+++ app/src/main/java/na/mo/ri/levelup/MyListAdapter.java
@@ -0,0 +1,60 @@
+package na.mo.ri.levelup;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MyListAdapter extends BaseAdapter {
+    Context context;
+    ArrayList<list_item> list_itemArrayList;
+
+    TextView nickname_textView;
+    TextView title_textView;
+    TextView date_textView;
+    TextView content_textView;
+    ImageView profile_imageView;
+
+    public MyListAdapter(Context context, ArrayList<list_item> list_itemArrayList) {
+        this.context = context;
+        this.list_itemArrayList = list_itemArrayList;
+    }
+    @Override
+    public int getCount() {
+        return this.list_itemArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list_itemArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView==null){
+            convertView = LayoutInflater.from(context).inflate(R.layout.list_item,null);
+            nickname_textView = (TextView)convertView.findViewById(R.id.nickname_textView);
+            content_textView = (TextView)convertView.findViewById(R.id.content_textView);
+            date_textView = (TextView)convertView.findViewById(R.id.date_textView);
+            title_textView  =(TextView)convertView.findViewById(R.id.title_textView);
+            profile_imageView = (ImageView)convertView.findViewById(R.id.profile_imageView);
+        }
+        nickname_textView.setText(list_itemArrayList.get(position).getNickname());
+        title_textView.setText(list_itemArrayList.get(position).getTitle());
+        content_textView.setText(list_itemArrayList.get(position).getContent());
+        date_textView.setText(list_itemArrayList.get(position).getWrite_date().toString());
+        profile_imageView.setImageResource(list_itemArrayList.get(position).getProfile_image());
+        return convertView;
+    }
+}
