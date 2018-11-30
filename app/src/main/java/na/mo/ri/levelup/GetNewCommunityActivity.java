d078760bc6bc4013fc329dbce7975a6c97531ece
namori3 <namori3@outlook.com>
2018-12-01 04:17:00 +0900

    Make some activities

---
+++ app/src/main/java/na/mo/ri/levelup/GetNewCommunityActivity.java
@@ -0,0 +1,13 @@
+package na.mo.ri.levelup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetNewCommunityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_new_community);
+    }
+}
