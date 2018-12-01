35d08c71b19d3ed88b5210e614766ec71640ac92
lee-seojune <gftr1219@ajou.ac.kr>
2018-12-01 20:12:19 +0900

    asdfs

---
+++ app/src/main/java/na/mo/ri/levelup/ActivitySubmain.java
@@ -0,0 +1,14 @@
+package na.mo.ri.levelup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySubmain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submain);
+        //ddddd
+    }
+}
