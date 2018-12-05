75bdb2e07c975e53f1432b7b21ecc0aaeed8987a
namori3 <namori3@outlook.com>
2018-12-05 22:37:17 +0900

    Some class

---
+++ app/src/main/java/na/mo/ri/levelup/CommunityData.java
@@ -0,0 +1,4 @@
+package na.mo.ri.levelup;
+
+class CommunityData {
+}
---
+++ app/src/main/java/na/mo/ri/levelup/UserData.java
@@ -0,0 +1,15 @@
+package na.mo.ri.levelup;
+
+class UserData {
+    private String name;
+    private String picLink;
+    private int level;
+    private String[] commuList;
+    private String[] shortGoal;
+    private String[] goal1, goal2, goal3;
+    private String[] longGoal;
+
+
+
+
+}
