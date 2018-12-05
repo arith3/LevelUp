546d33503f8ea9502a5a8ccd18424cd77922689f
namori3 <namori3@outlook.com>
2018-12-05 18:24:37 +0900

    Add codes

--- app/src/main/java/na/mo/ri/levelup/MainActivity.java
+++ app/src/main/java/na/mo/ri/levelup/MainActivity.java
@@ -1,14 +1,41 @@
 package na.mo.ri.levelup;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Button.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //ddddd
+        //main Login page
+
+        final EditText editID = (EditText) findViewById(R.id.editText);
+        final EditText editPS = (EditText) findViewById(R.id.editText2);
+
+        Button btnSin = (Button) findViewById(R.id.SignInBtn);
+        btnSin.setOnClickListener(this);
+        Button btnSup = (Button) findViewById(R.id.SignUpBtn);
+        btnSup.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.SignInBtn:
+                //
+                break;
+            case R.id.SignUpBtn:
+                //
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
