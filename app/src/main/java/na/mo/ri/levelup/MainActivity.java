c688e4d3c09d21ff28ecb92c40a0d25f0c052016
namori3 <namori3@outlook.com>
2018-12-05 19:27:41 +0900

    Comp MainAct

--- app/src/main/java/na/mo/ri/levelup/MainActivity.java
+++ app/src/main/java/na/mo/ri/levelup/MainActivity.java
@@ -2,13 +2,21 @@ package na.mo.ri.levelup;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity implements Button.OnClickListener {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
         btnSin.setOnClickListener(this);
         Button btnSup = findViewById(R.id.SignUpBtn);
         btnSup.setOnClickListener(this);
-
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -31,6 +39,19 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
                 final EditText editPS = findViewById(R.id.editText2);
                 Toast.makeText(this,editID.getText(), Toast.LENGTH_SHORT).show();
                 Toast.makeText(this,editPS.getText(), Toast.LENGTH_SHORT).show();
+
+                mAuth.signInWithEmailAndPassword(editID.getText().toString(), editPS.getText().toString()).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent it33 = new Intent(MainActivity.this, MyCommunityActivity.class);
+                            startActivity(it33);
+                        } else {
+                            Toast.makeText(MainActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
                 break;
             case R.id.SignUpBtn:
                 //
