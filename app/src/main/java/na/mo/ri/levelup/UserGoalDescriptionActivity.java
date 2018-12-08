0f0547d5ac28c39e2e947a15fba6af468c2b1612
lee-seojune <gftr1219@ajou.ac.kr>
2018-12-09 00:33:19 +0900

    화면연결 업데이트

--- app/src/main/java/na/mo/ri/levelup/UserGoalDescriptionActivity.java
+++ app/src/main/java/na/mo/ri/levelup/UserGoalDescriptionActivity.java
@@ -23,11 +23,11 @@ public class UserGoalDescriptionActivity extends AppCompatActivity {
         mReleation = (TextView) findViewById(R.id.releation);
         mGrowth = (TextView) findViewById(R.id.growth);
 
-        mSurvival.setOnClickListener(v -> showDialog("존재 욕구:목표 달성을 통해 어떤 자신의 가치를 발전시킬지 쓰쎄요 ex)프로그래밍을 통한 사치 향상"));
+        mSurvival.setOnClickListener(v -> showDialog("존재 욕구:\n목표 달성을 통해 어떤 자신의 가치를 발전시킬지 쓰시오 \nex)프로그래밍을 통한 사치 향상"));
 
-        mReleation.setOnClickListener(v -> showDialog("관계 욕구:LEVEL UP을 통해:자신의 인간관계에서 이번 목표 달성을 통해 어떤 부분을 얻고 싶은지 쓰시오. ex)주변사람의 인정"));
+        mReleation.setOnClickListener(v -> showDialog("관계 욕구:\nLEVEL UP을 통해:자신의 인간관계에서 이번 목표 달성을 통해 어떤 부분을 얻고 싶은지 쓰시오.\n ex)주변사람의 인정"));
 
-        mGrowth.setOnClickListener(v -> showDialog("성장 욕구:LEVEL UP을 통해 어떤 부분에서 발전하고 싶은지 쓰세요. ex) 취업을 위한 스팩 향상"));
+        mGrowth.setOnClickListener(v -> showDialog("성장 욕구:\nLEVEL UP을 통해 어떤 부분에서 발전하고 싶은지 쓰세요.\n ex) 취업을 위한 스팩 향상"));
 
 
     }
