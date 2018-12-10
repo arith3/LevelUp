2019511c0e1d920c82f3d50c819ab8ea4136d484
namori3 <namori3@outlook.com>
2018-12-11 05:26:39 +0900

    Change main logo

--- app/src/main/java/na/mo/ri/levelup/GuraMainActivity.java
+++ app/src/main/java/na/mo/ri/levelup/GuraMainActivity.java
@@ -25,7 +25,7 @@ public class GuraMainActivity extends AppCompatActivity implements Button.OnClic
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_gura_main);
 
         myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
